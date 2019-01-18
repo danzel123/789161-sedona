104ed714c81a370dc6a4fb46f98e95da4bdc11ab
Denis Krivoshspko <mrfisher123@mail.ru>
2019-01-18 12:55:47 +0800

    Почти финальные правки

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector(".search-hotel-btn");
+ var popup = document.querySelector(".search-form")
+popup.classList.add("modal-close")
+btn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-show")
+})
+  
