4654f50d1ad27d4ee5ddacce6637332cbbfd6684
Denis Krivoshspko <mrfisher123@mail.ru>
2019-01-20 17:01:11 +0800

    конец

--- js/min-main.js
+++ js/min-main.js
@@ -1,3 +1,2 @@
-var btn=document.querySelector(".search-hotel-btn");var popup=document.querySelector(".search-form")
-popup.classList.add("modal-close")
-btn.addEventListener("click",function(evt){evt.preventDefault();popup.classList.toggle("modal-show")})
+var btn=document.querySelector(".search-hotel-btn");var popup=document.querySelector(".search-form");var arrival=document.getElementById("arrival-date");var departure=document.getElementById("departure-date");var adults=document.getElementById("adults");var kids=document.getElementById("kids");popup.classList.add("modal-close")
+btn.addEventListener("click",function(evt){evt.preventDefault();popup.classList.toggle("modal-show");popup.classList.remove("modal-error")});popup.addEventListener("submit",function(evt){if(!arrival.value||!departure.value||!adults.value||!kids.value){evt.preventDefault();popup.classList.add("modal-error")}})
